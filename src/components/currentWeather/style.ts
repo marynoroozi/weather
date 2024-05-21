46ab6adb3fd39deb6ef7537d03d328c009f93e8b
mary <noroozi.m90@gmail.com>
2024-05-21 00:31:23 -0400

    Current weather style added

---
+++ src/components/currentWeather/style.ts
@@ -0,0 +1,13 @@
+export const useStyles = {
+    gridContainat: {
+      minWidth: 275,
+      display: "flex",
+      justifyContent: "center",
+      my: 10,
+    },
+    forcast: {
+      minWidth: 275,
+      display: "flex",
+      justifyContent: "center",
+    },
+  };
