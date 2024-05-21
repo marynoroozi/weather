88e7b117a8d1721d627fe3debd9b69590095ee13
mary <noroozi.m90@gmail.com>
2024-05-21 00:33:29 -0400

    Weather details style added

---
+++ src/components/weatherDetails/style.ts
@@ -0,0 +1,21 @@
+export const useStyles = {
+    container: {
+      justifyContent: "space-between",
+      display: "flex",
+      flexWrap: "wrap",
+      my: 5,
+    },
+    paper: {
+      display: "flex",
+      width: "97%",
+      padding: 1,
+      background:"#09898bbd",
+      my: 1,
+      py: 2,
+    },
+    paperImage: { display: "flex", alignItems: "center", color:"#fff" },
+    paperDetails: { display: "flex", flexDirection: "column", color:"#fff" },
+    cardContent: { flex: "1 0 auto" },
+    weatherIcon: { fontSize: "50px" },
+  };
+  
